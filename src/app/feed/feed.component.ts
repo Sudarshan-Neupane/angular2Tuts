23b759bcf40d26c998372ba860866c435f0f5b11
Sudarshan Neupane <sudarshan@Sudarshans-MBP.home>
2017-05-21 16:58:30 -0400

    retweet count added

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -7,12 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 tweets=[
-  {text:"This is my legend tweets",author:"sudarshan ",image:"one.jpeg"},
-  {text:"This is my legend2 tweets",author:"sudarshan ",image:"two.jpeg"},
-  {text:"Another author  tweets",author:"Gleen ",image:"three.jpeg"},
+  {text:"This is my legend tweets",author:"sudarshan ",image:"one.jpg","favorite":{"length":2},"retweets":["ram","shyam"]},
+  {text:"This is my legend2 tweets",author:"sugan ",image:"two.jpg","favorite":{"length":1},"retweets":["ram","shyam"]},
+  {text:"Another author  tweets",author:"Gleen ",image:"three.jpg","favorite":{"length":1},"retweets":["shyam"]},
 ]
   constructor() { }
+  isUserInCollection(collection: string[],userId: string) : boolean{
+    return collection.indexOf(userId)!=-1;
+  }
+  onFavorite(tweet){
 
+    tweet.favorite.length++;
+    // alert(tweet.favorite.length+1);
+  }
+  onRetweet(tweet){
+    if(!this.isUserInCollection(tweet.retweets,"Glen")) {
+      tweet.retweets.push("Glen");
+      // alert(tweet.favorite.length+1);
+    }
+
+  }
   ngOnInit() {
   }
 
